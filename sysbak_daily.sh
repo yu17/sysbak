7ed26657a50cbacd873373cdb07ed74b7b9a9341
yu17 <yjq17@hotmail.com>
2025-09-21 22:46:38 -0500

    Adding backup scripts to the repo.

---
+++ sysbak_daily.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#rsync -aHAXSP --info=backup0,copy0,del0,flist0,misc2,mount0,name0,progress0,remove0,skip0,stats2,symsafe --delete-delay --no-whole-file --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/var/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*/.gvfs"} / /mnt/System_img/prologue_backup/sysbak_daily
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Print Colored Logs
+log_info() {
+	echo -e "${GREEN}[sysbak]${NC} $1"
+}
+
+log_warning() {
+	echo -e "${YELLOW}[sysbak] Warning:${NC} $1"
+}
+
+log_error(){
+	echo -e "${RED}[sysbak] Error:${NC} $1"
+}
+
+# Configuration
+BACKUP_DIR="/mnt/PrivateD_0/not30_backup/sysbak_daily"
+SNAPSHOT_DIR="/.snapshots"
+
+mkdir -p $BACKUP_DIR
+
+# Create new backup
+# $1 - backup_path
+# $2 - backup_name (the actual name will be constructed as sysbak_$2 and/or sysbak_$2_update
+create_backup(){
+	local backup_path="$1"
+	local backup_name="sysbak_$2"
+
+	btrfs subvolume snapshot -r $backup_path ${SNAPSHOT_DIR}/${backup_name}_update
+	log_info "Daily Backup: Taking snapshot of $2($1) at ${SNAPSHOT_DIR}/${backup_name}_update"
+
+	if [ -e ${SNAPSHOT_DIR}/${backup_name} ] && [ -e ${BACKUP_DIR}/${backup_name} ]; then
+		log_info "Daily Backup: Incremental backup for $2($1)"
+		btrfs send -p ${SNAPSHOT_DIR}/${backup_name} ${SNAPSHOT_DIR}/${backup_name}_update | btrfs receive ${BACKUP_DIR}
+	else
+		log_warning "Daily Backup: Previous $2($1) backup not found! Creating new backup."
+		btrfs send ${SNAPSHOT_DIR}/${backup_name}_update | btrfs receive ${BACKUP_DIR}
+	fi
+
+	if [ -e ${SNAPSHOT_DIR}/${backup_name} ]; then
+		log_info "Daily Backup: Discarding previous daily backups ${SNAPSHOT_DIR}/${backup_name}"
+		btrfs subvolume delete -c ${SNAPSHOT_DIR}/${backup_name}
+	fi
+	if [ -e ${BACKUP_DIR}/${backup_name} ]; then
+		log_info "Daily Backup: Discarding previous daily backups ${BACKUP_DIR}/${backup_name}"
+		btrfs subvolume delete -c ${BACKUP_DIR}/${backup_name}
+	fi
+
+	log_info "Daily Backup: Renaming updated backups"
+	mv ${SNAPSHOT_DIR}/${backup_name}_update ${SNAPSHOT_DIR}/${backup_name}
+	mv ${BACKUP_DIR}/${backup_name}_update ${BACKUP_DIR}/${backup_name}
+
+	log_info "Daily Backup: Backup Finished!"
+}
+
+create_backup / root
+create_backup /home home
+sync
---
+++ sysbak_weekly.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Print Colored Logs
+log_info() {
+	echo -e "${GREEN}[sysbak]${NC} $1"
+}
+
+log_warning() {
+	echo -e "${YELLOW}[sysbak] Warning:${NC} $1"
+}
+
+log_error(){
+	echo -e "${RED}[sysbak] Error:${NC} $1"
+}
+
+# Configuration
+WEEKLY_DIR="/mnt/PrivateD_0/not30_backup/sysbak_weekly"
+DAILY_DIR="/mnt/PrivateD_0/not30_backup/sysbak_daily" 
+RETENTION_DAYS="60"
+MINIMAL_INTERVAL_DAYS="3"
+
+# Remove backups after a set time
+# $1 - backup name. The weekly backup filename is constucted as "sysbak_$1_$(date +%Y%m%dw%U)"
+discard_old_backups() {
+	log_info "Discarding backups of $1 older than ${RETENTION_DAYS} days..."
+
+	for file in ${WEEKLY_DIR}/sysbak_$1_*; do
+		# Extract the date part of the backup name.
+		file_date="${file#*sysbak_$1_}"
+		# Match the date part of the file name. (Sanity check)
+		if [[ "$file_date" =~ ^[0-9]{8}w[0-9]{2}$ ]]; then
+			# Truncate file date to the YYYYMMDD part (first 8 chars)
+			file_date="${file_date:0:8}"
+			file_date_sec=$(date -d "${file_date}" +%s 2>/dev/null)
+			current_date_sec=$(date +%s)
+			# Skip if failed to parse the date
+			if [ -z $file_date_sec ]; then
+				log_warning "Failed to parse date of file ${file}. Skipping."
+			else
+				# Calculate the age and delete the backup if necessary
+				age=$(( (current_date_sec - file_date_sec) / 86400 ))
+				if [ $age -gt $RETENTION_DAYS ]; then
+					log_info "Discarding backup: $file (age: $age days)"
+					#btrfs subvolume delete -c $file
+				else
+					log_info "Keeping backup: $file (age: $age days)"
+				fi
+			fi
+		fi
+	done
+}
+
+# Create new backup (via archiving the latest backup from sysbak_daily)
+# $1 - backup name. The daily backup is fetched using filename "sysbak_$1". The weekly backup filename is constucted as "sysbak_$1_$(date +%Y%m%dw%U)"
+create_backup() {
+	local backup_name="sysbak_$1"
+	# Ensure that the backup directory exists
+	mkdir -p ${WEEKLY_DIR}
+
+	# Skip if backups within $MINIMAL_INTERVAL_DAYS was found
+	for (( i=0; i<=$MINIMAL_INTERVAL_DAYS; i++ )) do
+		checkdate=$(date --date='-'${i}' day' +%Y%m%dw%U)
+		if [ -e ${WEEKLY_DIR}/${backup_name}_${checkdate} ]; then
+			log_warning "Rotative Backup Aborted: Recent backup (within ${MINIMAL_INTERVAL_DAYS} days) found!"
+			exit 0
+		fi
+	done
+
+	# Abort if daily backups do not exist
+	if ! [ -e ${DAILY_DIR}/${backup_name} ]; then
+		log_error "Rotative Backup Aborted: Daily backup not found!"
+		exit 1
+	fi
+
+	log_info "Rotative Backup: Creating backup ${backup_name}_$(date +%Y%m%dw%U)"
+	btrfs subvolume snapshot -r ${DAILY_DIR}/${backup_name} ${WEEKLY_DIR}/${backup_name}_$(date +%Y%m%dw%U)
+	log_info "Rotative Backup: Backup Finished Successfully!"
+}
+
+discard_old_backups root
+discard_old_backups home
+create_backup root
+create_backup home
+sync
